1fd37560692d140105f90aa5c525c30289cc6d56
Siobhan O'Donnell <sodonnell913@gmail.com>
2024-09-06 21:11:42 -0400

    Revised text on home page and changed project order

--- site/templates/info.php
+++ site/templates/info.php
@@ -11,10 +11,17 @@
             <li><?= $page->item5() ?></li>
             <li><?= $page->item6() ?></li>
             <li><?= $page->item7() ?></li>
-            <li><?= $page->item8() ?></li><br>
+            <li><?= $page->item8() ?></li>
+            <li><?= $page->item9() ?></li>
+            <li><?= $page->item10() ?></li>
+            <li><?= $page->item11() ?></li>
+            <li><?= $page->item12() ?></li><br>
         </ul>
         <p class="info-header"><?= $page->headline3() ?></p>
-        <p class="email"><?= $page->email() ?></p>
+        <p class="email"><?= $page->email() ?></p><br>
+        <ul class="services"><p class=info-header><?= $page->headline4() ?></p>
+            <li><?= $page->social1() ?></li>
+        </ul>
     </div>
 
 <?php snippet ('footer') ?>
